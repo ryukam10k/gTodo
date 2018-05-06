0d585dff14acc36da9baa67c9506419d7a85c353
ryukam10k <r.kamioka@gmail.com>
2018-05-06 21:42:48 +0900

    Initial Commit

---
+++ gTodo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gTodo
+//
+//  Created by 上岡隆也 on 2018/05/06.
+//  Copyright © 2018年 Qsworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
